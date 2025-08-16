bf1040ae805e2b386da0d60cc2120b98707ace70
Abdillahi Omar <abdillahiomardjamaainan@gmail.com>
2025-08-16 15:48:44 +0200

    ajout des codes

--- Untitled-1.py
+++ Untitled-1.py
@@ -8,3 +8,60 @@ c=a+b
 print(c)
 d=10
 print(d)
+w=4+5
+print(w)
+##Definition des types des données
+# Entier (int)
+# Flottant (float)
+# Chaîne de caractères (str)
+# Booléen (bool)
+a="Saad"
+type(a)
+print(type(a))
+
+##Explication complete sur les differents types des chaine de caractères existant
+# Les chaînes de caractères (str) en Python sont des séquences de caractères utilisées pour représenter du texte.
+# Elles peuvent être définies en utilisant des guillemets simples (' ') ou des guillemets doubles (" ").
+# Voici quelques exemples de chaînes de caractères :
+# 1. Chaîne de caractères simple
+nom="Abdillahi"
+prenom="Saad"
+# 2.chaine de caractere avec  des guillemets inclus
+phrase1="c'est une belle journée"
+print(phrase1)
+phrase2='il dit "bonjour"!'
+print(phrase2)
+##chaine des caractére speciaux
+##nouvelle ligne
+phrase3="bonjour\nPython"
+print(phrase3)
+o="c'est super"
+print(o)
+age=45
+T="j'ai"+" " + str(age) + " " + "ans"
+print(T)
+print(T[-1])
+print(T[0:5])
+print(T[0:5:2])  # Affiche les caractères de l'index 0 à 5 avec un pas de 2 
+p="ABDILLAHI OMAR DJAMA"
+print(p)
+print(p[0:5:3])  # 
+K="Python"
+print(K[2:6:3])
+
+O="ABDILLAHI"
+print(O[3:8:3])
+
+##Nombre entier
+##un nombre entier est un nombre sans virgule,peut etre negatif ou positif ou zero.
+l=5
+print(l)    
+##les differents operations possible
+V=9
+print(l+V)  # Addition
+print(l-V)  # Soustraction
+print(l*V)  # Multiplication
+print(l/V)  # Division
+print(l%V)  # Modulo (reste de la division) 
+print(l**V) # Puissance (l élevé à la puissance V)
+print(l//V) # Division entière (quotient de la division)
